9dd0373e7a78bbecb8a556fb6a4eb29662e08a4f
unaigomez <126858113+unaigomez@users.noreply.github.com>
2023-05-05 09:59:33 +0200

    canvis finals

---
+++ ElCaixerAutomaticFX/ElCaixerAutomaticFX/target/site/apidocs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.mycompany.elcaixerautomaticfx"}];updateSearchResults();
---
+++ ElCaixerAutomaticFX/ElCaixerAutomaticFX/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.mycompany.elcaixerautomaticfx","l":"com.mycompany.elcaixerautomaticfx"}];updateSearchResults();
